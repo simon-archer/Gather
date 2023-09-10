22fa41bbce8ed90660efa27685aa7bccf85c3fc5
SimonArcher <simonnline@gmail.com>
2023-09-10 13:50:27 +0200

    iOS compatible Audio player

--- islands/UnifiedIsland/AudioPlayer.ts
+++ islands/UnifiedIsland/AudioPlayer.ts
@@ -3,8 +3,12 @@ import { useEffect, useRef, useState } from "preact/hooks";
 import { tw } from "twind";
 
 export default function AudioPlayer({ textToConvert, textId, setIsLoading }) {
-  const audioRef = useRef(null);
-  const [audioUrl, setAudioUrl] = useState(null);
+  const audioRef = useRef(typeof Audio !== 'undefined' ? new Audio() : null);
+  const [audioUrl, setAudioUrl] = useState(
+    'https://hnshlqzjuzbgckjromvg.supabase.co/storage/v1/object/public/audioFiles/rU18Fk3uSDhmg5Xh41o4.mp3?t=2023-09-10T11:23:13.969Z'
+  );
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0); // Initialize the timer state
 
   useEffect(() => {
     if (!textToConvert || !textId) return;
@@ -37,14 +41,42 @@ export default function AudioPlayer({ textToConvert, textId, setIsLoading }) {
     fetchAndSetAudioUrl();
   }, [textToConvert, textId]);
 
-  return audioUrl && h("div", {
-    class: tw`flex flex-col justify-center items-center mt-8 bg-gray-100 rounded-lg shadow-md }`
-    }, [
-      h("audio", {
-        ref: audioRef,
-        src: audioUrl || "",  // If audioUrl is null or undefined, use an empty string
-        controls: true,
-        disabled: !audioUrl,  // Disable the audio player if audioUrl is not available
-      }),
-  ]);
+  useEffect(() => {
+    if (audioUrl && audioRef.current) {
+      audioRef.current.src = audioUrl;
+    }
+  }, [audioUrl]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.addEventListener('timeupdate', () => {
+        setCurrentTime(audioRef.current.currentTime);
+      });
+    }
+
+    // Cleanup
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.removeEventListener('timeupdate', () => {});
+      }
+    };
+  }, []);
+
+  const handlePlayPause = () => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.pause();
+      } else {
+        audioRef.current.play();
+      }
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  return h("div", { class: tw`flex flex-col justify-center items-center mt-8 bg-gray-100 rounded-lg shadow-md` },
+    [
+      h("button", { onClick: handlePlayPause }, isPlaying ? "Pause" : "Play"),
+      h("span", {}, `Current Time: ${currentTime.toFixed(2)}s`) // Display the timer
+    ]
+  );
 }
